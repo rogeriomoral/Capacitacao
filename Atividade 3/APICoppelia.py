e2b9122f0665f1eab2d098a13665dcaead12ab3c
LeilaFarias <leilafarias1324@gmail.com>
2024-07-11 16:11:47 -0300

    implementa atividade 3

---
+++ Atividade 3/APICoppelia.py
@@ -0,0 +1,34 @@
+from coppeliasim_zmqremoteapi_client import *
+import numpy as np 
+
+client = RemoteAPIClient()
+sim = client.getObject('sim')
+
+handle_robo = sim.getObject('/robozao')
+
+roda_direita = sim.getObject('/robozao/motor_direito')
+roda_esquerda = sim.getObject('/robozao/motor_esquerdo')
+
+# Dados do robo P3DX
+# L = diametro do chassi
+L = 0.17
+# r = raio da roda
+r = 0.02
+
+
+wr = np.deg2rad(270)
+wl = np.deg2rad(-270)
+
+sim.setJointTargetVelocity(roda_direita, wr)
+sim.setJointTargetVelocity(roda_esquerda, wl)
+
+sim.startSimulation()
+
+while (t := sim.getSimulationTime()) < 10:
+
+    pos = sim.getObjectPosition(handle_robo, -1)
+    print(pos)
+
+
+
+sim.stopSimulation()
